70088e96deadd3534027016f35b04fa1a07c0970
DataSpell-demo <you@example.com>
2025-07-27 23:45:19 +0200

    Configure project settings and add SQL queries
    Added IntelliJ IDEA project configuration files (.idea/*) for project setup, including database connections and Python SDK configurations. Introduced new SQL query files (e.g., Films.sql) to analyze film data, such as cast size and actor details.

---
+++ Films.sql
@@ -0,0 +1,10 @@
+SELECT * FROM actor JOIN public.film_actor fa on actor.actor_id = fa.actor_id;
+
+SELECT f.film_id,
+       f.title,
+       COUNT(fa.actor_id) as cast_size
+FROM film f
+         JOIN film_actor fa ON f.film_id = fa.film_id
+GROUP BY f.film_id, f.title
+ORDER BY cast_size DESC
+LIMIT 1;
