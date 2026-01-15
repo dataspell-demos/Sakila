607d901f502bf2ad840adabbec3b05333e3314dd
DataSpell-demo <you@example.com>
2026-01-15 08:55:57 +0100

    New commit

--- Films.sql
+++ Films.sql
@@ -1,10 +1,2 @@
-SELECT * FROM "table" JOIN public.film_actor fa on actor.actor_id = fa.actor_id;
-
-SELECT f.film_id,
-       f.title,
-       COUNT(fa.actor_id) as cast_size
-FROM film f
-         JOIN film_actor fa ON f.film_id = fa.film_id
-GROUP BY f.film_id, f.title
-ORDER BY cast_size DESC
-LIMIT 1;
+select *
+from actor;
