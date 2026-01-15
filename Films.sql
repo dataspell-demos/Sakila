50627506f30e11cfa9b8cc180e44b0916fe470f7
DataSpell-demo <you@example.com>
2026-01-15 08:59:08 +0100

    In which films Penelope stared

--- Films.sql
+++ Films.sql
@@ -3,3 +3,10 @@ from actor;
 
 select *
 from actor join public.film_actor fa on actor.actor_id = fa.actor_id;
+
+SELECT f.title
+FROM film f
+         JOIN film_actor fa ON f.film_id = fa.film_id
+         JOIN actor a ON a.actor_id = fa.actor_id
+WHERE a.first_name = 'PENELOPE'
+  AND a.last_name = 'GUINESS';
