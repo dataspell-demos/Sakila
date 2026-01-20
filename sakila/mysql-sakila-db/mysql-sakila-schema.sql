b7b7f509d89585d3350ab5ce069be02920f0493d
dataspell-demos <153208766+dataspell-demos@users.noreply.github.com>
2026-01-20 12:00:38 +0100

    Delete table structures for category, city, and country
    Removed table definitions for category, city, and country.

--- sakila/mysql-sakila-db/mysql-sakila-schema.sql
+++ sakila/mysql-sakila-db/mysql-sakila-schema.sql
@@ -57,37 +57,18 @@ CREATE TABLE address (
 -- Table structure for table `category`
 --
 
-CREATE TABLE category (
-  category_id INT UNSIGNED NOT NULL AUTO_INCREMENT,
-  name VARCHAR(25) NOT NULL,
-  last_update TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
-  PRIMARY KEY  (category_id)
-)ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
 
 --
 -- Table structure for table `city`
 --
 
-CREATE TABLE city (
-  city_id INT UNSIGNED NOT NULL AUTO_INCREMENT,
-  city VARCHAR(50) NOT NULL,
-  country_id INT UNSIGNED NOT NULL,
-  last_update TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
-  PRIMARY KEY  (city_id),
-  KEY idx_fk_country_id (country_id),
-  CONSTRAINT `fk_city_country` FOREIGN KEY (country_id) REFERENCES country (country_id) ON DELETE RESTRICT ON UPDATE CASCADE
-)ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
 
 --
 -- Table structure for table `country`
 --
 
-CREATE TABLE country (
-  country_id INT UNSIGNED NOT NULL AUTO_INCREMENT,
-  country VARCHAR(50) NOT NULL,
-  last_update TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
-  PRIMARY KEY  (country_id)
-)ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 --
 -- Table structure for table `customer`
